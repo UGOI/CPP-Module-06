c095ca1f72abf747c0a6425f67a6a1ef21f580e1
Stefan Dukic <sdukic@3-B-11.42heilbronn.de>
2023-02-22 18:21:37 +0100

    fix: in ex02 add try catch block to identify

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -29,14 +29,19 @@ void identify(Base* p) {
 }
 
 void identify(Base& p) {
-    if (dynamic_cast<A*>(&p) != nullptr) {
-        std::cout << "A" << std::endl;
-    } else if (dynamic_cast<B*>(&p) != nullptr) {
-        std::cout << "B" << std::endl;
-    } else if (dynamic_cast<C*>(&p) != nullptr) {
-        std::cout << "C" << std::endl;
+    try {
+        if (dynamic_cast<A*>(&p) != nullptr) {
+            std::cout << "A" << std::endl;
+        } else if (dynamic_cast<B*>(&p) != nullptr) {
+            std::cout << "B" << std::endl;
+        } else if (dynamic_cast<C*>(&p) != nullptr) {
+            std::cout << "C" << std::endl;
+        }
+    } catch (std::bad_cast& ex) {
+        std::cerr << "dynamic_cast failed: " << ex.what() << std::endl;
     }
 }
+
 
 int main() {
     Base* obj = generate();
